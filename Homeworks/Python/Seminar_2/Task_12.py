ec474f5b9c28087e724978f7feb320b5f5bdddd2
Artur Khaibiev <khaybiev@gmail.com>
2023-02-27 16:17:45 +0300

    Added Workshop Lesson_3 and PDF files

--- Homeworks/Python/Seminar_2/Task_12.py
+++ Homeworks/Python/Seminar_2/Task_12.py
@@ -6,8 +6,30 @@
 summ = int(input('Vvedite summu chisel: '))
 multiply = int(input('Vvedite proizvedenie chisel: '))
 
-discriminant = summ ** 2 - 4 * multiply
-x = (summ + discriminant ** (1 / 2)) // 2
+discriminant = summ ** 2 - 4 * multiply 
+x = (summ + discriminant ** (1 / 2)) / 2
 y = summ - x
 
 print(f'x = {x}, y = {y}')
+
+for i in range(summ):
+    for j in range(multiply):
+        if i + j == summ and i * j == multiply:
+            print(i, j) 
+            break
+    else:
+        continue
+    break
+else:
+    print('Решения нет.')
+
+s = int(input('введите сумму двух чисел: '))
+p = int(input('введите произведение двух чисел: '))
+
+for x in range(1, 1000):
+    if s - x == p // x and p % x == 0:
+        print(x, p // x)
+        break
+else:
+    print('решения нет')
+
