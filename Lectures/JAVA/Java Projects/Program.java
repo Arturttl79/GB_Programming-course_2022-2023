5a7f8ed2f1674e17ac44a15e68c78986c184a3a5
Artur Khaibiev <khaybiev@gmail.com>
2023-03-23 11:31:00 +0100

    Added lecture 1 presentation for JAVA

---
+++ Lectures/JAVA/Java Projects/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        int a = 123;
+        a = a - a;
+        System.out.println("Hello, World!");
+        System.out.println(a);
+    }
+}
