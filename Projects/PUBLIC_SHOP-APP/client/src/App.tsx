2df5ece89c3cdeb9aa776fc29488ca16e95e1017
Artur Khaibiev <khaybiev@gmail.com>
2023-09-02 23:39:39 +0100

    PUBLIC_SHOP-APP 002 Added headline

--- Projects/PUBLIC_SHOP-APP/client/src/App.tsx
+++ Projects/PUBLIC_SHOP-APP/client/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './layouts/NavigationAndFooter/NavigationBar';
-import HomePage from './layouts/HomePage/HomePage';
-import Footer from './layouts/NavigationAndFooter/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <HomePage />
-      <Footer />
     </div>
   );
 }
